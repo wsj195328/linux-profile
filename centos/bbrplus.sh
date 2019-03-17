3da3d2d7ec840697a7f7fb7375bb12c33572ea66
291876969@qq.com <291876969@qq.com>
2019-03-17 13:33:31 +0800

    JC_Update: 1、

--- centos/bbrplus.sh
+++ centos/bbrplus.sh
@@ -16,11 +16,11 @@ echo -e "卸载加速..."
 sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
 sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
 if [[ -e /appex/bin/serverSpeeder.sh ]]; then
-    wget --no-check-certificate -O appex.sh https://raw.githubusercontent.com/0oVicero0/serverSpeeder_Install/master/appex.sh && chmod +x appex.sh && bash appex.sh uninstall
+    wget --no-check-certificate -O appex.sh https://raw.githubusercontent.com/wsj195328/linux-profile/master/speeder/appex.sh && chmod +x appex.sh && bash appex.sh uninstall
     rm -f appex.sh
 fi
 echo -e "下载内核..."
-wget https://github.com/cx9208/bbrplus/raw/master/centos7/x86_64/kernel-${kernel_version}.rpm
+wget https://github.com/wsj195328/linux-profile/tree/master/kernel/kernel-${kernel_version}.rpm
 echo -e "安装内核..."
 yum install -y kernel-${kernel_version}.rpm
 
