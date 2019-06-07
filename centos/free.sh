9533f54fda0298fcfc3ae98990c1450d3aa1dca4
JCwang <wsj195328@outlook.com>
2019-06-08 02:46:35 +0800

    Update free.sh

--- centos/free.sh
+++ centos/free.sh
@@ -1,2 +1,2 @@
 # when memory less 400, free memory
-free -m |grep -i mem |awk '{if($4 < 340){ printf("3") > "/proc/sys/vm/drop_caches"}}';
+free -m |grep -i mem |awk '{if($4 < 280){ printf("3") > "/proc/sys/vm/drop_caches"}}';
