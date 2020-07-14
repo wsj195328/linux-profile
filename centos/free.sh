902aa7c241f30bd4c04557a1c7c56c1e22d559b6
JCwang <wsj195328@outlook.com>
2020-07-15 00:31:44 +0800

    Update free.sh
    add

--- centos/free.sh
+++ centos/free.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-free -m |grep -i mem |awk '{if($4 < 280){ printf("3") > "/proc/sys/vm/drop_caches"}}';
+free -m |grep -i mem |awk '{if($4 < 780){ printf("3") > "/proc/sys/vm/drop_caches"}}';
